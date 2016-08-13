9161c9a5e950f94930fb7b8312705412bcc4ce6b
Reilly Villanueva <reillyvillanueva@gmail.com>
2016-08-12 17:33:31 -0700

    Almost Finished TOfHanoi

--- towersOfHanoi.js
+++ towersOfHanoi.js
@@ -9,17 +9,38 @@ class TGame {
     this.towers = [[3,2,1],[],[]];
   }
 
+  print () {
+    console.log(JSON.stringify(this.towers));
+  }
+
   promptMove (cb) {
-    console.log(this.towers);
+    this.print();
     reader.question("What piece do ya want?", function (answer1) {
       reader.question("What tower to put it on?", function (answer2) {
         const startTowerIdx = answer1;
         const endTowerIdx = answer2;
-
         cb(answer1, answer2);
       });
     });
-    console.log(this.startTowerIdx, this.endTowerIdx);
+  }
+
+  move(sT, eT) {
+    if (this.isValidMove(sT, eT)) {
+      let transfer = this.towers[sT].pop();
+      this.towers[eT].push(transfer);
+      return true;
+    }
+    alert("you rock at this game");
+    return false;
+  }
+
+  isWon() {
+    console.log("here");
+    console.log(this.towers[0] == [3,2]);
+    if (this.towers[0].length === 0 || (this.towers[2] === [3,2,1])) {
+      return true;
+    }
+    return false;
   }
 
   isValidMove(sT, eT) {
@@ -34,7 +55,23 @@ class TGame {
     return true;
   }
 
-  run() {
+  run(completionCallback) {
+    // if(this.isWon() === true) {
+    //   completionCallback();
+    // }
+    // if(this.isWon() === false) {
+    //   this.promptMove(this.move.bind(this));
+    // }
+    this.promptMove((sT, eT) => {
+      this.move(sT, eT);
+      console.log(this.towers[1], this.towers[2]);
+      if(this.isWon()) {
+        completionCallback();
+      } else {
+        this.run(completionCallback);
+      }
+    });
+    // this.run(completionCallback);
     // until towers two || three are identical to tower one start
       // get a move from player
       // check validity of move
@@ -45,9 +82,14 @@ class TGame {
 }
 
 let game = new TGame();
+game.run(() => {
+  reader.close();
+  console.log("You won, good job!");
+});
 // game.promptMove( (a,b) => {(console.log(a,b));
 // reader.close(); });
-
-console.log(game.isValidMove(0,1));
-console.log(game.isValidMove(1,0));
-reader.close();
+// console.log(game.move(0,1));
+// console.log(game.move(2,0));
+// console.log(game.move(1,2));
+// console.log(game.isValidMove(0,1));
+// console.log(game.isValidMove(1,0));
